792c985611a12e1d249a7fafb1354eb1599012f9
rwema <rwema916@gmail.com>
2025-01-26 07:57:27 +0200

    condtructors

--- constructors.dart
+++ constructors.dart
@@ -8,6 +8,19 @@
 void main(){
     dynamic student1 = Student(23, "rwema");
     print("${student1.id}, ${student1.name}");
+    student1.study();
+    student1.sleep();
+
+    var student2 = Student(45, "nobii");
+    print("${student2.id}, ${student2.name}");
+
+    var student3 = Student.myCustomConstructor();
+      student3.id = 5;
+      student3.name = "Hirwa";
+      print("${student3.id} and ${student3.name}");
+
+      var student4 = Student.myAnotherNamedConstructor(1, "Boy");
+      print("${student4.id} and ${student4.name}");
 
 }
 
@@ -16,4 +29,19 @@ class Student {
   String? name;
 
    Student(this.id, this.name);
+
+   Student.myCustomConstructor(){
+    print("This is my custom constructor");
+   }
+
+   Student.myAnotherNamedConstructor(this.id, this.name);
+
+   void study(){
+      print("This $name is studying");
+   }
+
+   void sleep(){
+      print("This $name is sleeping");
+   }
+
 }
